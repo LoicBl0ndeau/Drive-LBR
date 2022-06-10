b615b60821ef92ac170c07c24ce704bc9e72aec8
Martin CAPELLE <martin.capelle@student.junia.com>
2022-06-10 16:30:53 +0200

    edit + add restriction il maintenant impossible d'ajouter ou modifier le mail d'un utilisateur qui est déjà connue dans la bdd

--- account_Manager_submit_edit_user.php
+++ account_Manager_submit_edit_user.php
@@ -34,32 +34,7 @@ $Role = $postData['Role'];
 
 ?>
 
-<?php
-try
-{
-	$mysqlClient = new PDO('mysql:host=localhost;dbname=lbr;charset=utf8', 'root');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
 
-// Ecriture de la requête
-$sqlQuery = 'UPDATE profil SET email = :email, Nom = :Nom, Prenom = :Prenom, Description = :Description, Role = :Role WHERE Id_Profil = :Id_Profil';
-
-// Préparation
-$edited_user = $mysqlClient->prepare($sqlQuery);
-
-// Exécution ! l'utilisateur est maintenant en base de données
-$edited_user->execute([
-    'Id_Profil' => $Id_Profil,
-    'email' => $Email,
-    'Nom' => $Nom,
-    'Prenom' => $Prenom,
-    'Description' => $Description,
-    'Role' => $Role,
-]);
-?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -81,7 +56,66 @@ $edited_user->execute([
       <?php include_once('account_Manager_header.php'); ?>
     </header>
 
-      <div class="container">
+		<?php
+
+		// Validation du formulaire
+		$same_email = 0;
+			foreach ($users as $user) {
+			    if ($user['email'] === $Email) {
+						$same_email++;
+			        //echo "le mail semble déjà utilisé";
+			    }
+					else {
+						//echo "le mail semble ne jamais avoir été utilisé";
+			      //$errorMessage = sprintf('le mail semble ne jamais avoir été utilisé');
+			    }
+			}
+			//echo $same_email;
+		?>
+
+		<?php
+		if ($same_email) {
+			$errorMessage = sprintf('L\'adresse email semble déjà utilisé, l\'utilisateur n\'est pas enregistré');
+		}
+		else {
+			try
+			{
+				$mysqlClient = new PDO('mysql:host=localhost;dbname=lbr;charset=utf8', 'root');
+			}
+			catch (Exception $e)
+			{
+			        die('Erreur : ' . $e->getMessage());
+			}
+
+			// Ecriture de la requête
+			$sqlQuery = 'UPDATE profil SET email = :email, Nom = :Nom, Prenom = :Prenom, Description = :Description, Role = :Role WHERE Id_Profil = :Id_Profil';
+
+			// Préparation
+			$edited_user = $mysqlClient->prepare($sqlQuery);
+
+			// Exécution ! l'utilisateur est maintenant en base de données
+			$edited_user->execute([
+			    'Id_Profil' => $Id_Profil,
+			    'email' => $Email,
+			    'Nom' => $Nom,
+			    'Prenom' => $Prenom,
+			    'Description' => $Description,
+			    'Role' => $Role,
+			]);
+		}
+		
+		?>
+
+    <div class="container">
+
+			<!-- si message d'erreur on l'affiche -->
+	    <?php if(isset($errorMessage)) : ?>
+	        <div class="alert alert-danger" role="alert">
+	            <?php echo $errorMessage; ?>
+	        </div>
+					<a class="btn btn-primary" href="account_Manager_accueil.php">Retour au gestionnaire</a>
+
+			<?php else: ?>
 
         <h1>Utilisateur bien modifié</h1>
 
@@ -97,14 +131,17 @@ $edited_user->execute([
           </div>
 	        <a class="btn btn-primary" href="account_Manager_accueil.php">Retour au gestionnaire</a>
         </div>
-      </div>
 
-			<!-- Page Profil -->
-			<?php include_once('mask_profil.php'); ?>
+			<?php endif; ?>
 
-			<?php
-				echo "<script>$('#name').text('".$_SESSION['loggedUser']['Prenom']." ".$_SESSION['loggedUser']['Nom']."');$('#role').text('".$_SESSION['loggedUser']['Role']."');</script>";
-			?>
+    </div>
+
+		<!-- Page Profil -->
+		<?php include_once('mask_profil.php'); ?>
+
+		<?php
+			echo "<script>$('#name').text('".$_SESSION['loggedUser']['Prenom']." ".$_SESSION['loggedUser']['Nom']."');$('#role').text('".$_SESSION['loggedUser']['Role']."');</script>";
+		?>
 
 	</body>
 </html>
