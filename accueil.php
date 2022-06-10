fb50eef1ca8e1ab8b5c24d6ff550cf5a5355784c
LoicBl0ndeau <loic.blondeau@student.junia.com>
2022-06-10 10:39:04 +0200

    Importer aux petis oignons

--- accueil.php
+++ accueil.php
@@ -17,14 +17,9 @@
 					$allowedExtensions = ['ogm', 'wmv', 'mpg', 'webm', 'ogv', 'mov', 'asx', 'mpeg', 'mp4', 'm4v', 'avi','jpg', 'jpeg', 'gif', 'png', 'tiff', 'pjp', 'jfif', 'bmp', 'svg', 'xbm', 'dib', 'jxl', 'svgz', 'webp', 'ico', 'tif', 'pjpeg', 'avif'];
 					$extension = strtolower(pathinfo($_FILES['media']['name'][$i])['extension']);
 					if(in_array($extension, $allowedExtensions)){ //on vérifie que l'extension est un média
-						//$req=$PDO->query("SHOW TABLE STATUS FROM Id_fichier LIKE 'fichier'");
-						//$donnees = $req->fetch();
-						//echo $donnees['Auto_increment'];
-
-
-						$req=$PDO->query("SELECT Id_fichier FROM fichier ORDER BY Id_fichier DESC");
-						$res = $req->fetchAll();
-						$Id_fichier = $res[0]["Id_fichier"] + 1;
+						$req=$PDO->query("SHOW TABLE STATUS FROM lbr LIKE 'fichier'");
+						$res = $req->fetch();
+						$Id_fichier = $res['Auto_increment'];
 						mkdir("upload/".$Id_fichier, 0700);
 						$nomFichier = basename($_FILES["media"]["name"][$i]);
 						move_uploaded_file($_FILES["media"]["tmp_name"][$i], "upload/".$Id_fichier."/".$nomFichier);
