928f51a28da9ca006b9a4da7434f7be9880de8a3
Martin CAPELLE <martin.capelle@student.junia.com>
2022-06-15 15:40:38 +0200

    à la bonne heure + le fameux retour du boutton retour

--- account_Manager_delete_user.php
+++ account_Manager_delete_user.php
@@ -69,7 +69,7 @@ $users = $userStatement->fetchAll();
 				<form action="account_Manager_submit_delete_user.php" method="post">
         	<button class="btn btn-danger" type="submit" name="user" value="<?php echo $Id_Profil ?>">SUPPRIMER</button>
 				</form>
-        <button class="btn btn-success" type="button" name="button">RETOUR</button>
+        <button class="btn btn-success" type="button" name="button" onclick="window.location.href='http://localhost/drive_lbr/account_Manager_accueil.php';">RETOUR</button>
     </div>
   </body>
 </html>
