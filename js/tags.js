2eac630e1b9a37e3c04869479081f7d5947e2f13
Mell0wPill0w <106145294+Mell0wPill0w@users.noreply.github.com>
2022-06-22 15:30:32 +0200

    tags js modif

--- js/tags.js
+++ js/tags.js
@@ -16,16 +16,17 @@ $(document).ready(function(){
   $('#mask_tag').on("click",function(){
     $('#ajoutcat').css("transform","translate(-100vw,-50%)");
     $('#mask_tag').css("transform","translateX(-100vw)");
-    $('*:not(#mask_tag,html,body, #ajoutcat,#ajoutcat *)').css("filter","blur(0)");
+    $('#ajouttag').css("transform","translate(-100vw,-50%)");
+    $('*:not(html,body)').css("filter","blur(0)");
   });
   $('#plus_tag').on("click",function(){
     $('#ajouttag').css("transform","translate(0,-50%)");
     $('#mask_tag').css("transform","translateX(0)");
     $('*:not(#mask_tag,html,body, #ajouttag,#ajouttag *)').css("filter","blur(2px)");
   });
-  $('#mask_tag').on("click",function(){
+  /*$('#mask_tag').on("click",function(){
     $('#ajouttag').css("transform","translate(-100vw,-50%)");
     $('#mask_tag').css("transform","translateX(-100vw)");
     $('*:not(#mask_tag,html,body, #ajouttag,#ajouttag *)').css("filter","blur(0)");
-  });
+  });*/
   });
