b0260bd7a25c394a0fc2d4642c5d6171e6f6d4d4
Loïc_B <loic.blondeau@student.junia.com>
2022-06-24 22:39:47 +0200

    maj afficher les tags dans les infos de la corbeille

--- corbeille.php
+++ corbeille.php
@@ -150,7 +150,18 @@
 					$reqEmail = $PDO->prepare("SELECT * FROM profil WHERE Id_Profil=?");
 					$reqEmail->execute(array($media['Auteur_Id']));
 					$resEmail = $reqEmail->fetchAll();
-					$appendInfos = "<div class='container_informations' id_media='container_inf_".$media['Id_fichier']."'><br /><h2 class='menu_informations'>Informations <span class='fermer_informations'>✖</span></h2><br />Nom: ".$media['Titre']."<br /><br />Auteur: <span class='mail_auteurs' style='display: none;'>".$resEmail[0]['Prenom']." ".$resEmail[0]['Nom']." (".$resEmail[0]['Description'].")</span>".$resEmail[0]['email']."<br /><br />Date d'ajout: <span class='date_ajout'>".$media['Date_de_publication']."</span>".date('d/m/Y',strtotime($media['Date_de_publication']))."<br /><br />Taille: ".round(0.000001*$media['Taille'], 2)." Mo (".$media['Taille']." octets)<br /><br />Tags: <br /></div>";
+					$reqTags = $PDO->prepare("SELECT * FROM caractériser WHERE Id_fichier=?");
+					$reqTags->execute(array($media['Id_fichier']));
+					$resTags = $reqTags->fetchAll();
+					$list_tags = "";
+					$reqNomTags = $PDO->prepare("SELECT * FROM tag WHERE Id_Tag=?");
+					foreach ($resTags as $tag) {
+						$reqNomTags->execute(array($tag['Id_Tag']));
+						$resNomTags = $reqNomTags->fetch();
+						$list_tags .= $resNomTags['Nom'].", ";
+					}
+					$list_tags = substr($list_tags,0,-2);
+					$appendInfos = "<div class='container_informations' id_media='container_inf_".$media['Id_fichier']."'><br /><h2 class='menu_informations'>Informations <span class='fermer_informations'>✖</span></h2><br />Nom: ".$media['Titre']."<br /><br />Auteur: <span class='mail_auteurs' style='display: none;'>".$resEmail[0]['Prenom']." ".$resEmail[0]['Nom']." (".$resEmail[0]['Description'].")</span>".$resEmail[0]['email']."<br /><br />Date d'ajout: <span class='date_ajout'>".$media['Date_de_publication']."</span>".date('d/m/Y',strtotime($media['Date_de_publication']))."<br /><br />Taille: ".round(0.000001*$media['Taille'], 2)." Mo (".$media['Taille']." octets)<br /><br />Tags: <span class='liste_des_tags'>".$list_tags."</span><br /></div>";
 					echo <<<END
 						<script>$('body').append("{$appendInfos}")</script>
 						END;
