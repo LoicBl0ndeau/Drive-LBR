61a67d44c2560dbe94dd9f3166b4ca69894e6ae0
LoicBl0ndeau <loic.blondeau@student.junia.com>
2022-06-24 13:24:51 +0200

    Réglage d'un bug au moment d'ouvrir le menu profil

--- mask_profil.php
+++ mask_profil.php
@@ -126,12 +126,12 @@
   $('#pdp_user').on("click",function(){
     $('#profil').css("transform","translate(0,-50%)");
     $('#mask_profil').css("transform","translateX(0)");
-    $('*:not(#mask_profil,html,body, #profil,#profil *)').css("filter","blur(2px)");
+    $('*:not(#mask_profil,html,body, #container_header_right, header, #profil,#profil *)').css("filter","blur(2px)");
   });
   $('#mask_profil').on("click",function(){
     $('#profil').css("transform","translate(100vw,-50%)");
     $('#mask_profil').css("transform","translateX(100vw)");
-    $('*:not(#mask_profil,html,body, #profil,#profil *)').css("filter","blur(0)");
+    $('*:not(#mask_profil,html,body, #profil,#profil *)').css("filter","");
   });
   $('#container_deconnexion *').on("click",function(){
     window.location.replace("logout.php");
