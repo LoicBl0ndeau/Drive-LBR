5f1a299e4d44ec065851a3d07739e65c93e02b2d
Martin CAPELLE <martin.capelle@student.junia.com>
2022-06-24 21:38:09 +0200

    account_Manager_header_lite.php

---
+++ account_Manager_header_lite.php
@@ -0,0 +1,10 @@
+<?php
+    include_once('account_Manager_variables.php');
+?>
+
+<a href="accueil.php"><img src="images/logoLONGUEURBlanc.png" alt="logo_longueur_blanc" id="logo_longueur_blanc" /></a>
+
+
+<div id="container_header_right">
+	<img src="images/pdp_user.jpg" alt="pdp_utilisateur" id="pdp_user" />
+</div>
