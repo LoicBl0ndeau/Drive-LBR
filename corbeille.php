ee3ca805730d9bf8b1551f5bf728c444dbe50b73
Loïc_B <loic.blondeau@student.junia.com>
2022-06-24 22:22:43 +0200

    mini maj de la corbeille

--- corbeille.php
+++ corbeille.php
@@ -63,15 +63,7 @@
 		<form method="post" id="restore" enctype="multipart/form-data"><input type="hidden" name="restore" /><input type="hidden" name="randomrestoreOK" value="<?php echo $_SESSION['random_OK']; ?>" /></form>
 		<div id="lecteur"></div>
 		<header>
-			<a href="accueil.php"><img src="images/logoLONGUEURBlanc.png" alt="logo_longueur_blanc" id="logo_longueur_blanc" /></a>
-			<div id="scearch_bar">
-				<input type="text" id="text_scearch_bar" placeholder='Tapez ".mp4;2021" par exemple.' />
-				<label for="text_scearch_bar">
-					<svg fill="#FFFF" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">
-						<path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/>
-					</svg>
-				</label>
-			</div>
+			<a href="accueil.php"><img src="images/logoLONGUEURBlanc.png" alt="logo_longueur_blanc" class="logo_longueur_blanc" /></a>
 			<div id="container_header_right">
 				<img src="images/pdp_user.jpg" alt="pdp_utilisateur" id="pdp_user" />
 			</div>
